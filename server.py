a805def0688aabd0919f563953fcb5a7ed5adb09
Thyagchlzn <thyagchlzn@gmail.com>
2023-08-07 20:34:57 +0530

    third

--- server.py
+++ server.py
@@ -6,14 +6,16 @@ app = Flask(__name__)
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return 'hello world!'
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
-def add(number):
+def add():
+    number=request.json
     return json.dumps({'result':number['first']+number['second']})
 
 @app.route("/calculator/subtract", methods=['POST'])
-def subtract(number):
+def subtract():
+    number=request.json
     return json.dumps({'result':number['first']-number['second']})
 
 
